836191dbfd613e590b862c1c38225296e658b786
汪淼 <1071597946@qq.com>
2017-03-06 14:41:38 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by foreverlove on 17/3/6.
+//  Copyright © 2017年 foreverlove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
